e2a0e77664b8e906fff56950a99c1c5261d2e6ca
mitchellm <developer@mitchellmudd.dev>
2023-05-17 08:11:30 -0400

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_API.Controllers;
+
+[ApiController]
+[Route("weather")]
+public class WeatherForecastForMeController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastForMeController> _logger;
+
+    public WeatherForecastForMeController(ILogger<WeatherForecastForMeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "dumbWeather")]
+    public IEnumerable<WeatherForecastForMe> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecastForMe
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
+
---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace Test_API;
+
+public class WeatherForecastForMe
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
